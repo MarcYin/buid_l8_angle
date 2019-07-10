5070493e10c79165ce98544f9ddf8b1abf3980d1
MarcYin <marcollinbobo@gmail.com>
2019-07-10 15:23:28 +0100

    first commit

---
+++ conda_upload.sh
@@ -0,0 +1,9 @@
+conda install conda-build  
+conda install anaconda-client 
+USER=f0xy  
+conda config --set anaconda_upload no  
+conda build . -c conda-forge
+export CONDA_PACKAGE=`conda build --output . -c conda-forge | grep bz2`
+echo $CONDA_PACKAGE
+ls -lah $CONDA_PACKAGE
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
